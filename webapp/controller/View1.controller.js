00c4269dc32ad0bdeaf6690c5b3577a67665f4c2
Arun Kumar <arundarling174@gmail.com>
2024-10-22 11:13:59 +0000

    my new chan

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,74 +1,116 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "infy/akm/project1/controls/customControl"
 ],
 
-//Arun 
+    //Arun 
 
 
-// Testadd
+    // Testadd
 
-function (Controller) {
-    "use strict";
+    function (Controller,MessageToast,customControl) {
+        "use strict";
 
-    return Controller.extend("infy.akm.project1.controller.View1", {
-        onInit: function () {
-            var odatamodel = this.getOwnerComponent().getModel()
-            this.getView().setModel(odatamodel)
-        },
-        onPressGo:function(oEvent){
+        return Controller.extend("infy.akm.project1.controller.View1", {
+            onInit: function () {
+                var that = this
+                var odatamodel = this.getOwnerComponent().getModel()
+                this.getView().setModel(odatamodel)
+            },
+            onPressGo: function (oEvent) {
+                debugger;
 
+            },
+            onNavPress: function (oEvent) {
+                debugger;
+                var router = this.getOwnerComponent().getRouter()
+                router.navTo('route2', {
+                    data: "123"
+                })
+            },
+            onBeforeRebindTable1: function (oEvent) {
+                debugger;
+                var key = this.getView().byId('_mysmtFtr').getControlByKey('Discontinued').getSelectedKey()
+                var value = this.getView().byId('_mysmtFtr').getControlByKey('ProductID')
+                var filter = []
+                if (key) {
+                    var afilter = new sap.ui.model.Filter('Discontinued', sap.ui.model.FilterOperator.EQ, key)
+                    oEvent.getParameter('bindingParams').filters.push(afilter)
+                }
+                if (value) {
+                    value = value.mProperties._semanticFormValue.replaceAll('=', '')
 
-        },
-        onNavPress:function(oEvent){
-debugger;
- var router = this.getOwnerComponent().getRouter()
- router.navTo('route2',{
-    data:"123"
- })
-        },
-        onBeforeRebindTable1:function(oEvent){
-            debugger;
-            var key = this.getView().byId('_mysmtFtr').getControlByKey('Discontinued').getSelectedKey()
-            var value = this.getView().byId('_mysmtFtr').getControlByKey('ProductID')
-           var filter = []
-              if(key){
-               var afilter = new sap.ui.model.Filter('Discontinued',sap.ui.model.FilterOperator.EQ,key)
-               filter.push(afilter)
-            }
-               if(value){ 
-                 value = value.mProperties._semanticFormValue.replaceAll('=','')
-
-                 var length = value.split(',').length
-                 for(var a=0;a<length;a++){
-                     var searchkey = value.split(',')[a]
-                     searchkey ? filter.push(new sap.ui.model.Filter('ProductID',sap.ui.model.FilterOperator.EQ,searchkey)) : ''
-                     
-       
-                 }
+                    var length = value.split(',').length
+                    for (var a = 0; a < length; a++) {
+                        var searchkey = value.split(',')[a]
+                        searchkey ?  oEvent.getParameter('bindingParams').filters.push(new sap.ui.model.Filter('ProductID', sap.ui.model.FilterOperator.EQ, searchkey)) : ''
 
 
+                    }
+
+
+
+                }
+
+
+            },
+
+            onNavtoApp: function (oEvent) {
+                var intent = 'Action-toappperssample'
+                var oXappnav = sap.ushell.Container.getService("CrossApplicationNavigation")
+                oXappnav.ttoExternal({
+                    target: {
+                        semanticObject: "Action",
+                        action: "toappperssample"
+                    },
+                    params: {
+
+                    }
+
+                })
+            },
+
+
+            onfilterChange: function () {
+                debugger
+            },
+
+            oninitise: function (oEvent) {
+               var smtfilter =  this.getView().byId('_mysmtFtr')
+                var value  = this.getView().byId('_mysmtFtr').getControlByKey('ProductID')
+                value.setValue(2)
+                smtfilter.search()
+            },
+
+            onaftervload: function (oevnt) {
+             debugger;
                 
+            },
+            onaftervSve: function (oevnt) {
+                debugger
+            },
+            onbeforevFetch: function (oevnt) {
+                debugger
+            },
+            onbeforevS: function (oevnt) {
+                var value =  this.getView().byId('_mysmtFtr').getControlByKey('ProductID').getValue()
+                if(value){
+                    var obj = {
+                        ProductID : 1
+                    }
+                    oevnt.getParameter('context').setPropery('/custumdata',obj)
+                    MessageToast.show('Done')}
+                else{
+                oevnt.preventDefault()    
+            return;
             }
-                oEvent.getParameter('bindingParams').filters = filter       
-
-
             },
-             
-            onNavtoApp:function(oEvent){
-                var intent =  'Action-toappperssample'           
-                 var oXappnav = sap.ushell.Container.getService("CrossApplicationNavigation")
-                 oXappnav.ttoExternal({
-          target:{
-    semanticObject: "Action",
-                    action: "toappperssample"
-          },
-          params:{
-
-          }
-
-                 })
+            oninitized: function (oevnt) {
+                debugger
             },
-             
+
+
+        });
     });
-});
 
--- webapp/controller/View2.controller.js
+++ webapp/controller/View2.controller.js
@@ -1,30 +1,56 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("infy.akm.project1.controller.View2", {
-        onInit: function () {
-            debugger;
-            var router = this.getOwnerComponent().getRouter()
+        return Controller.extend("infy.akm.project1.controller.View2", {
+            onInit: function () {
+                debugger;
+                var sPath = jQuery.sap.getModulePath("infy.akm.project1.model", "/data.json");
+      var oModel = new sap.ui.model.json.JSONModel();
+      oModel.loadData(sPath, null, false);
+      this.getView().setModel(oModel, "TestModel");
+
+      // Log the data to ensure it's loaded correctly
+      console.log(oModel.getData());
+            },
+            onbeforebindTable: function (oEvent) {
+                var afilters = []
+                var birthkey = this.getView().byId('_IDGenDatePicker').getDateValue()
+
+                if (birthkey) {
+                    //    birthkey = birthkey.getTime()
+                    //    birthkey="/Date("+birthkey+")/"
+                    var filter = new sap.ui.model.Filter('BirthDate', sap.ui.model.FilterOperator.EQ, birthkey)
+                    oEvent.getParameter('bindingParams').filters.push(filter)
+                }
+
+            },
+
+            onclick: function (oEvent) {
+
+                var spath = jQuery.sap.getModulePath('infy.akm.project1.model', '/data.json')
+                var omodel = new sap.ui.model.json.JSONModel()
+                omodel.loadData(spath, "", false)
+                this.getView().setModel(omodel, "TestModel")
+            },
+            onSelect: function (oEvent) {
+            },
+            onopen: function (oEvent) {
+             
+              
+                    oEvent.getSource()._oCalendar.mAggregations.month[0].setShowWeekNumbers(false)
+
+               
           
-        },
-        onbeforebindTable:function(oEvent){
-           var afilters = []
-            var birthkey =this.getView().byId('_IDGenDatePicker').getDateValue()
 
-          if(birthkey)
-       {
-        //    birthkey = birthkey.getTime()
-        //    birthkey="/Date("+birthkey+")/"
-            var filter = new sap.ui.model.Filter('BirthDate',sap.ui.model.FilterOperator.EQ,birthkey)
-             oEvent.getParameter('bindingParams').filters.push(filter)
-       }
- 
-        },
+            },
+            onnavigate: function (oEvent) {
+            }
 
+
+
+        })
 
     })
-
-})
